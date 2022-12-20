2a4a3608c6036ee8dddacf498dce8453013afd2c
LottaViljamaa <lotta.viljamaa@tuni.fi>
2022-12-20 20:03:25 +0200

    Create second page

---
+++ todo-app/src/SecondPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from './TodoList';
+
+
+function ToinenSivu() {
+  return (
+    <div className="Muistilista">
+      <TodoList/>
+    </div>
+  )
+}
+
+export default ToinenSivu;
