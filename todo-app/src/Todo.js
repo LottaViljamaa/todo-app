2d8739ea81827447949c6bedada4ff4e9bd6bf75
LottaViljamaa <lotta.viljamaa@tuni.fi>
2022-12-20 22:09:58 +0200

    Add buttons to edit and move tasks

--- todo-app/src/Todo.js
+++ todo-app/src/Todo.js
@@ -2,12 +2,16 @@ import React from "react";
 
 function Todo(todos) {
 
-  console.log(todos);
-
   return todos.map((todo) => (
     <div>
       <div>{todo.text}</div>
       <div>#{todo.tag}</div>
+      <button>Move up</button>
+      <button>&#8593;</button>
+      <button>Edit</button>
+      <button>Delete</button>
+      <button>&#8595;</button>
+      <button>Move down</button>
     </div>
   ))
 }
