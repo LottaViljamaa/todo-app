c0e74efe9d8e29ee924c6623a32b013c59ec8b76
LottaViljamaa <lotta.viljamaa@tuni.fi>
2022-12-20 20:45:03 +0200

    Create function and input field to add new todo

--- todo-app/src/Todos.js
+++ todo-app/src/Todos.js
@@ -1,11 +1,26 @@
 import React from "react";
 import {useState} from "react";
 
-function Todos() {
+function Todos(props) {
+
+  const [input, setInput] = useState();
+  const [tagInput, setTagInput] = useState("");
+
+  //Takes user input
+  const userInput = e => {
+    setInput(e.target.value);
+  }
 
   return(
     <div>
-      
+      <input
+      type="text"
+      placeholder="Write a new task"
+      value={input}
+      name="text"
+      onChange={userInput}
+      required
+      ></input>
     </div>
   )
 }
