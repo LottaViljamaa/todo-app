4cf143b2880f22f59d01290c0748e5cde87f827c
LottaViljamaa <lotta.viljamaa@tuni.fi>
2022-12-20 20:35:28 +0200

    Create Todo function

---
+++ todo-app/src/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {useState} from "react";
+import Todos from "./Todos";
+
+function Todo() {
+
+
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+export default Todo;
