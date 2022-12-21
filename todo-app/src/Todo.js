3100512b062ab112421ab4aab7a55c68f14cbdbe
LottaViljamaa <lotta.viljamaa@tuni.fi>
2022-12-21 12:24:37 +0200

    Add moveToTop function to button

--- todo-app/src/Todo.js
+++ todo-app/src/Todo.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Todos from "./Todos"
 
-function Todo({todos, deleteTodo, moveUp, moveDown}) {
+function Todo({todos, deleteTodo, moveToTop, moveUp, moveDown}) {
 
   return todos.map((todo, index) => (
     <div>
       <div>{todo.text}</div>
       <div>#{todo.tag}</div>
-      <button>Move up</button>
+      <button
+      onClick={() => moveToTop(todos, index)}
+      >Move to top</button>
       <button
         onClick={() => moveUp(todos, index)}
         >&#8593;
@@ -20,7 +22,7 @@ function Todo({todos, deleteTodo, moveUp, moveDown}) {
       <button
       onClick={() => moveDown(todos, index)}
       >&#8595;</button>
-      <button>Move down</button>
+      <button>Move to bottom</button>
     </div>
   ))
 }
