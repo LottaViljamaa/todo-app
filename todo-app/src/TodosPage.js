6be2d2da10138218a09e3d2665147eba1a98052f
LottaViljamaa <lotta.viljamaa@tuni.fi>
2022-12-28 19:53:46 +0200

    Rename secondpage to TodosPage

---
+++ todo-app/src/TodosPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from './TodoList';
+
+
+function SecondPage() {
+  return (
+    <div className="todoList">
+      <TodoList/>
+    </div>
+  )
+}
+
+export default SecondPage;
